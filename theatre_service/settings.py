418fdace05fd60d366ee3594819724765100cd69
Yeva Yepifanova <yevayepifanova@gmail.com>
2024-08-08 00:51:46 +0300

    Add apps theatre, user, AUTH_USER_MODEL to settings.py

--- theatre_service/settings.py
+++ theatre_service/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "theatre",
+    "user",
 ]
 
 MIDDLEWARE = [
@@ -100,6 +102,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "user.User"
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
